df06fca1439b2b62f9676f49716f233bd6a34ca4
Marcel Meyer <mm.marcelmeyer@gmail.com>
2025-07-21 00:28:44 +0200

    Remove ignored file operation

--- internal/audio/file_creator.go
+++ internal/audio/file_creator.go
@@ -96,96 +96,96 @@ func NewFileCreator(
 	}, nil
 }
 
-func (c *FileCreator) RemoveOtherFiles() error {
-	return removeOtherFiles(c.outputDir, c.outputFilesToKeep)
+func (f *FileCreator) RemoveOtherFiles() error {
+	return removeOtherFiles(f.outputDir, f.outputFilesToKeep)
 }
 
-func (c *FileCreator) TextToAudioFile(ctx context.Context, segments []Segment, name string) error {
-	file, op, err := c.textToAudioFile(ctx, segments, name)
+func (f *FileCreator) TextToAudioFile(ctx context.Context, segments []Segment, name string) error {
+	file, op, err := f.textToAudioFile(ctx, segments, name)
 	if err != nil {
 		return err
 	}
-	path := filepath.Join(c.outputDir, file)
+	path := filepath.Join(f.outputDir, file)
 
-	c.outputFilesToKeep[path] = true
+	f.outputFilesToKeep[path] = true
 
 	slog.Info(op.String()+"\t", "path", path)
 
 	return nil
 }
 
-func (c *FileCreator) textToAudioFile(ctx context.Context, segments []Segment, name string) (string, fileOperation, error) {
-	concatWavFile, _, err := c.toWavConcatenated(ctx, segments)
+func (f *FileCreator) textToAudioFile(ctx context.Context, segments []Segment, name string) (string, fileOperation, error) {
+	concatWavFile, err := f.toWavConcatenated(ctx, segments)
 	if err != nil {
 		return "", 0, err
 	}
 
-	switch c.audioFormat {
+	switch f.audioFormat {
 	case Wav:
-		return c.copyWav(concatWavFile, name)
+		return f.copyWav(concatWavFile, name)
 	case M4a:
-		return c.afconvert(ctx, concatWavFile, name)
+		return f.afconvert(ctx, concatWavFile, name)
 	case Mp3:
-		return c.ffmpeg(ctx, concatWavFile, name)
+		return f.ffmpeg(ctx, concatWavFile, name)
 	default:
 		return "", 0, errors.New("unsupported audio format")
 	}
 }
 
-func (c *FileCreator) toWavConcatenated(ctx context.Context, segments []Segment) (string, fileOperation, error) {
+func (f *FileCreator) toWavConcatenated(ctx context.Context, segments []Segment) (string, error) {
 	if len(segments) == 1 {
-		return c.toWav(ctx, segments[0])
+		return f.toWav(ctx, segments[0])
 	}
 
 	wavFiles := make([]string, len(segments))
 	for i, s := range segments {
-		wavFile, _, err := c.toWav(ctx, s)
+		wavFile, err := f.toWav(ctx, s)
 		if err != nil {
-			return "", 0, err
+			return "", err
 		}
-		wavFiles[i] = filepath.Join(c.tempDir, wavFile)
+		wavFiles[i] = filepath.Join(f.tempDir, wavFile)
 	}
 
-	return c.concat(ctx, wavFiles...)
+	return f.concat(ctx, wavFiles...)
 }
 
-func (c *FileCreator) toWav(ctx context.Context, s Segment) (string, fileOperation, error) {
+func (f *FileCreator) toWav(ctx context.Context, s Segment) (string, error) {
 	switch v := s.(type) {
 	case *Sound:
-		return c.extendLength(ctx, v.value(), v.len())
+		return f.extendLength(ctx, v.value(), v.len())
 	case *Text:
-		return c.textToWav(ctx, v)
+		return f.textToWav(ctx, v)
 	case *Silence:
-		return c.silence(ctx, v.len())
+		return f.silence(ctx, v.len())
 	case *Group:
 		values := v.values()
 		if len(values) == 0 {
-			return c.silence(ctx, v.len())
+			return f.silence(ctx, v.len())
 		}
-		con, _, err := c.toWavConcatenated(ctx, values)
+		con, err := f.toWavConcatenated(ctx, values)
 		if err != nil {
-			return "", 0, err
+			return "", err
 		}
-		return c.extendLength(ctx, con, v.len())
+		return f.extendLength(ctx, con, v.len())
 	default:
-		return "", 0, errors.New("unknown Segment type")
+		return "", errors.New("unknown Segment type")
 	}
 }
 
-func (c *FileCreator) textToWav(ctx context.Context, s *Text) (string, fileOperation, error) {
+func (f *FileCreator) textToWav(ctx context.Context, s *Text) (string, error) {
 	if s.value() == "" {
-		return c.silence(ctx, s.len())
+		return f.silence(ctx, s.len())
 	}
-	file, _, err := c.ttsCmd(ctx, s.value())
+	file, _, err := f.ttsCmd(ctx, s.value())
 	if err != nil {
-		return "", 0, err
+		return "", err
 	}
 
-	return c.extendLength(ctx, file, s.len())
+	return f.extendLength(ctx, file, s.len())
 }
 
-func (c *FileCreator) useExistingFile(path string) (bool, fileOperation, error) {
-	for _, paths := range c.existingFiles {
+func (f *FileCreator) useExistingFile(path string) (bool, fileOperation, error) {
+	for _, paths := range f.existingFiles {
 		for _, p := range paths {
 			if norm.NFC.String(p) == path {
 				return true, skipped, nil
@@ -193,7 +193,7 @@ func (c *FileCreator) useExistingFile(path string) (bool, fileOperation, error) 
 		}
 	}
 
-	paths, ok := c.existingFiles[extractHash(path)]
+	paths, ok := f.existingFiles[extractHash(path)]
 	if ok {
 		// If the hash exists the is at least one path in the list.
 		err := copyFile(paths[0], path)
@@ -205,16 +205,16 @@ func (c *FileCreator) useExistingFile(path string) (bool, fileOperation, error) 
 	return false, noop, nil
 }
 
-func (c *FileCreator) ttsCmd(ctx context.Context, text string) (string, fileOperation, error) {
-	switch c.tts.TTSCmd {
+func (f *FileCreator) ttsCmd(ctx context.Context, text string) (string, fileOperation, error) {
+	switch f.tts.TTSCmd {
 	case Say:
-		return c.say(ctx, c.tts.Voice, text)
+		return f.say(ctx, f.tts.Voice, text)
 	case EspeakNG:
-		return c.espeakNG(ctx, c.tts.Voice, text)
+		return f.espeakNG(ctx, f.tts.Voice, text)
 	case Custom:
-		return c.custom(ctx, text)
+		return f.custom(ctx, text)
 	default:
-		return "", 0, fmt.Errorf("unknown tts command %v", c.tts.TTSCmd)
+		return "", 0, fmt.Errorf("unknown tts command %v", f.tts.TTSCmd)
 	}
 }
 
@@ -310,13 +310,13 @@ func listFiles(dir string) (map[string]bool, error) {
 	return files, nil
 }
 
-func (c *FileCreator) copyWav(srcFile, name string) (string, fileOperation, error) {
-	srcWavPath := filepath.Join(c.tempDir, srcFile)
+func (f *FileCreator) copyWav(srcFile, name string) (string, fileOperation, error) {
+	srcWavPath := filepath.Join(f.tempDir, srcFile)
 
 	filename := name + ".wav"
-	dstWavPath := filepath.Join(c.outputDir, filename)
+	dstWavPath := filepath.Join(f.outputDir, filename)
 
-	exists, op, err := c.useExistingFile(dstWavPath)
+	exists, op, err := f.useExistingFile(dstWavPath)
 	if err != nil {
 		return "", 0, err
 	}
@@ -359,63 +359,63 @@ func extractHash(filename string) string {
 
 // extendLength does not use cmd because files would be read everytime
 // to get the hash. If nothing is to do reading every file consumes time (~1s vs. instant).
-func (c *FileCreator) extendLength(ctx context.Context, file string, extendedLength time.Duration) (string, fileOperation, error) {
+func (f *FileCreator) extendLength(ctx context.Context, file string, extendedLength time.Duration) (string, error) {
 	if extendedLength == 0 {
-		return file, noop, nil
+		return file, nil
 	}
 
 	ext := filepath.Ext(file)
 	nameNoExt := strings.TrimSuffix(file, ext)
 
 	filePadded := fmt.Sprintf("%s_extended-%s-%s%s", nameNoExt, extendedLength, hashShort(file, extendedLength), ext)
-	filePaddedPath := filepath.Join(c.tempDir, filePadded)
+	filePaddedPath := filepath.Join(f.tempDir, filePadded)
 
-	exists, op, err := c.useExistingFile(filePaddedPath)
+	exists, _, err := f.useExistingFile(filePaddedPath)
 	if err != nil {
-		return "", 0, err
+		return "", err
 	}
 	if exists {
-		return filePadded, op, nil
+		return filePadded, nil
 	}
 
-	length, err := c.length(ctx, file)
+	length, err := f.length(ctx, file)
 	if err != nil {
-		return "", 0, err
+		return "", err
 	}
 
 	addLength := extendedLength - length
 	if addLength <= 0 {
-		return file, 0, nil
+		return file, nil
 	}
 
 	arguments := []string{
-		filepath.Join(c.tempDir, file),
+		filepath.Join(f.tempDir, file),
 		filePaddedPath,
 		"pad", "0", fmt.Sprintf("%f", addLength.Seconds()),
 	}
 
 	slog.Debug("execute", "cmd", strings.Join(append([]string{"sox"}, arguments...), " "))
 
-	out, err := c.execCmdCtx(
+	out, err := f.execCmdCtx(
 		ctx,
 		"sox",
 		arguments...,
 	).CombinedOutput()
 	if err != nil {
-		return "", 0, cmdError("sox", arguments, out)
+		return "", cmdError("sox", arguments, out)
 	}
-	return filePadded, created, nil
+	return filePadded, nil
 }
 
-func (c *FileCreator) length(ctx context.Context, file string) (time.Duration, error) {
+func (f *FileCreator) length(ctx context.Context, file string) (time.Duration, error) {
 	arguments := []string{
 		"--i",
 		"-D",
-		filepath.Join(c.tempDir, file),
+		filepath.Join(f.tempDir, file),
 	}
 
 	slog.Debug("execute", "cmd", strings.Join(append([]string{"sox"}, arguments...), " "))
-	out, err := c.execCmdCtx(
+	out, err := f.execCmdCtx(
 		ctx,
 		"sox",
 		arguments...,
@@ -437,42 +437,44 @@ func (c *FileCreator) length(ctx context.Context, file string) (time.Duration, e
 	return time.Duration(float * float64(time.Second)), nil
 }
 
-func (c *FileCreator) concat(ctx context.Context, filenames ...string) (string, fileOperation, error) {
-	return c.cmd(
+func (f *FileCreator) concat(ctx context.Context, filenames ...string) (string, error) {
+	file, _, err := f.cmd(
 		ctx,
 		args{
 			cmd:              "sox",
 			argsBeforeOutput: filenames,
-			outPath:          c.tempDir,
+			outPath:          f.tempDir,
 			outFilename:      "concat",
 			outFileExt:       "wav",
 		})
+	return file, err
 }
 
 // https://billposer.org/Linguistics/Computation/SoxTutorial.html#silence
-func (c *FileCreator) silence(ctx context.Context, duration time.Duration) (string, fileOperation, error) {
+func (f *FileCreator) silence(ctx context.Context, duration time.Duration) (string, error) {
 	if duration <= 0 {
-		return "", 0, errors.New("negative or zero duration for silence")
+		return "", errors.New("negative or zero duration for silence")
 	}
-	return c.cmd(
+	file, _, err := f.cmd(
 		ctx,
 		args{
 			cmd:              "sox",
 			argsBeforeOutput: []string{"-n", "-r", "22050"},
-			outPath:          c.tempDir,
+			outPath:          f.tempDir,
 			outFilename:      fmt.Sprintf("silence_%s", duration),
 			outFileExt:       "wav",
 			argsAfterOutput:  []string{"trim", "0.0", fmt.Sprintf("%.2f", duration.Seconds())},
 		})
+	return file, err
 }
 
-func (c *FileCreator) say(ctx context.Context, voice string, text string) (string, fileOperation, error) {
+func (f *FileCreator) say(ctx context.Context, voice string, text string) (string, fileOperation, error) {
 	// `--data-format=LEF32@22050` is needed for wav.
 	// https://stackoverflow.com/questions/9729153/error-on-say-when-output-format-is-wave
 	// The comments state that a sample rate higher than 22050 is not recommended.
 	dataFormat := "LEF32@22050"
 
-	return c.cmd(
+	return f.cmd(
 		ctx,
 		args{
 			cmd: "say",
@@ -481,28 +483,28 @@ func (c *FileCreator) say(ctx context.Context, voice string, text string) (strin
 				"--voice", voice,
 				"--output-file",
 			},
-			outPath:         c.tempDir,
+			outPath:         f.tempDir,
 			outFilename:     "say",
 			outFileExt:      "wav",
 			argsAfterOutput: []string{text},
 		})
 }
 
-func (c *FileCreator) espeakNG(ctx context.Context, voice string, text string) (string, fileOperation, error) {
-	return c.cmd(
+func (f *FileCreator) espeakNG(ctx context.Context, voice string, text string) (string, fileOperation, error) {
+	return f.cmd(
 		ctx,
 		args{
 			cmd:              "espeak-ng",
 			argsBeforeOutput: []string{"-v", voice, "-out"},
-			outPath:          c.tempDir,
+			outPath:          f.tempDir,
 			outFilename:      "espeak-ng",
 			outFileExt:       "wav",
 			argsAfterOutput:  []string{text},
 		})
 }
 
-func (c *FileCreator) custom(ctx context.Context, text string) (string, fileOperation, error) {
-	cmd := c.tts.Voice
+func (f *FileCreator) custom(ctx context.Context, text string) (string, fileOperation, error) {
+	cmd := f.tts.Voice
 	if err := checkFmtArg(cmd, "%[1]s"); err != nil {
 		return "", 0, err
 	}
@@ -512,9 +514,9 @@ func (c *FileCreator) custom(ctx context.Context, text string) (string, fileOper
 	}
 
 	name := fmt.Sprintf("%s-%s.wav", strings.SplitN(cmd, " ", 2)[0], hashShort(cmd, text))
-	path := filepath.Join(c.tempDir, name)
+	path := filepath.Join(f.tempDir, name)
 
-	exists, op, err := c.useExistingFile(path)
+	exists, op, err := f.useExistingFile(path)
 	if err != nil {
 		return "", 0, err
 	}
@@ -540,23 +542,23 @@ func checkFmtArg(cmd, fmtArg string) error {
 	return nil
 }
 
-func (c *FileCreator) ffmpeg(ctx context.Context, wavFile string, name string) (string, fileOperation, error) {
-	return c.cmd(
+func (f *FileCreator) ffmpeg(ctx context.Context, wavFile string, name string) (string, fileOperation, error) {
+	return f.cmd(
 		ctx,
 		args{
 			cmd:              "ffmpeg",
 			argsBeforeInput:  []string{"-i"},
-			inputPath:        c.tempDir,
+			inputPath:        f.tempDir,
 			inputFilename:    wavFile,
 			argsBeforeOutput: []string{"-ab", "256k", "-ar", "44100", "-ac", "2"},
-			outPath:          c.outputDir,
+			outPath:          f.outputDir,
 			outFilename:      name,
 			outFileExt:       "mp3",
 		})
 }
 
-func (c *FileCreator) afconvert(ctx context.Context, wavFile string, name string) (string, fileOperation, error) {
-	return c.cmd(
+func (f *FileCreator) afconvert(ctx context.Context, wavFile string, name string) (string, fileOperation, error) {
+	return f.cmd(
 		ctx,
 		args{
 			cmd: "afconvert",
@@ -569,9 +571,9 @@ func (c *FileCreator) afconvert(ctx context.Context, wavFile string, name string
 				"--quality", "127",
 				"--strategy", "2",
 			},
-			inputPath:     c.tempDir,
+			inputPath:     f.tempDir,
 			inputFilename: wavFile,
-			outPath:       c.outputDir,
+			outPath:       f.outputDir,
 			outFilename:   name,
 			outFileExt:    "m4a",
 		})
@@ -615,7 +617,7 @@ func (a *args) outFile() (string, error) {
 	) + "." + a.outFileExt, nil
 }
 
-func (c *FileCreator) cmd(ctx context.Context, args args) (string, fileOperation, error) {
+func (f *FileCreator) cmd(ctx context.Context, args args) (string, fileOperation, error) {
 	outName, err := args.outFile()
 	if err != nil {
 		return "", 0, err
@@ -623,7 +625,7 @@ func (c *FileCreator) cmd(ctx context.Context, args args) (string, fileOperation
 
 	outPath := filepath.Join(args.outPath, outName)
 
-	exists, op, err := c.useExistingFile(outPath)
+	exists, op, err := f.useExistingFile(outPath)
 	if err != nil {
 		return "", 0, err
 	}
@@ -635,7 +637,7 @@ func (c *FileCreator) cmd(ctx context.Context, args args) (string, fileOperation
 	concatArgs := slices.Concat(args.argsBeforeInput, inPath, args.argsBeforeOutput, []string{outPath}, args.argsAfterOutput)
 	slog.Debug("execute", "cmd", strings.Join(append([]string{args.cmd}, concatArgs...), " "))
 
-	out, err := c.execCmdCtx(
+	out, err := f.execCmdCtx(
 		ctx,
 		args.cmd,
 		concatArgs...,
